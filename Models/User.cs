5329201e4a94346b0b7c4d3e1e6e82470776e978
Harshana Perera <harshanatxn@gmail.com>
2019-11-10 17:09:07 +0530

    User Model Added

---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [MinLength(3)]
+        [Required]
+        public string Name { get; set; }
+        
+        [Range(1,121)]
+        public int  Age { get; set; }
+        
+        [Phone]
+        [Required]
+        public string PhoneNumber { get; set; }
+        
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+    }
+}
